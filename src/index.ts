98983f366e9eaf3340716302abc87e153e4025bf
Maya Saringan <mayas.saringan@gmail.com>
2021-10-30 17:40:30 -0400

    Add storage upload helper

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,28 @@ import firebase from "firebase";
 import * as auth from "./auth";
 import * as firestore from "./firestore";
 import * as error from "./error";
+import * as storage from "./storage";
 export { default as Provider } from "./Provider";
 export type { default as Metadata } from "./types/Metadata";
 export type { default as Account } from "./types/Account";
 export { default as AccountReducer } from "./redux/account";
 export * from "./redux/account";
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-export const initialize = (config: Object): void => {
+export const initialize = (config: Record<string, string>): void => {
   if (!firebase.apps.length) {
     firebase.initializeApp(config);
   }
 };
+
+let config: Record<string, string> = {};
 const firebaseHelper = {
-  initialize,
+  initialize: (_config: Record<string, string>): void => {
+    config = _config;
+    initialize(config);
+  },
   auth,
   firestore,
   error,
+  storage,
 };
 export default firebaseHelper;
